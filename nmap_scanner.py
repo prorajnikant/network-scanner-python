aa594dd3d8aabf2400df757b0a907ec6dd2925f3
prorajnikant <harshitkochar17@gmail.com>
2020-12-27 02:26:01 +0530

    Base code with command line args

--- nmap_scanner.py
+++ nmap_scanner.py
@@ -1,7 +1,7 @@
+#!usr/bin/evn python3
 import nmap
+import argparse
 
-#initialize the port scanner
-nmScan = nmap.PortScanner()
 
 def nmap_scanner(ip,port_range):
 
@@ -22,4 +22,24 @@ def nmap_scanner(ip,port_range):
 				print(f"Port : {port}\tState : {nmScan[host][protocol][port]['state']}")
 
 
-nmap_scanner('127.0.0.1','21-443')
+def main():
+	#initialize the port scanner
+	nmScan = nmap.PortScanner()
+	parser = argparse.ArgumentParser(description="python scan_nmap.py  --host=127.0.0.1  --port=21")
+
+	parser.add_argument('--host',action="store",dest = "host" , required=True)
+	parser.add_argument('--port',action="store",dest = "port" ,type = int,required=True)
+	
+	args = parser.parse_args()
+	host = args.host
+	port = args.port
+
+	if host == None or port == None:
+		print(parser.usage)
+	else:
+		print(f"Scanning : {host} - {port}")
+
+	nmap_scanner(host,port)
+
+if __name__ == '__main__':
+	main()
